8fd00dc2bb8fada1746f9e303f44a2769c5a9221
Shravani Siddhul <120581113+shravanisiddhul@users.noreply.github.com>
2024-10-05 15:31:37 +0000

    Changes to be committed: 	new file:   .devcontainer/01_basics/01_variables.js 	new file:   .devcontainer/01_basics/02_datatypes.js 	renamed:    .devcontainer/01_basics/test.js -> .github/test.js

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 32451;
+let accountEmail = "shravani@gmail.com;"
+var accountPassword = "1234567";
+accountCity = "Solapur"
+let accountState;
+// accountId = 34;  //not allowed
+
+accountEmail = "shrav@gmail.com"
+
+accountPassword = "1231232";
+
+accountCity = "Pune";
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
