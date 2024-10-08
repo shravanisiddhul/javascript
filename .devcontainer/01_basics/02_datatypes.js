5bb570ae6b868b81ea655ecdf370c433d2548027
Shravani Siddhul <120581113+shravanisiddhul@users.noreply.github.com>
2024-10-08 12:01:12 +0000

    Changes to be committed: 	modified:   .devcontainer/01_basics/02_datatypes.js

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 3
+) ;//code readablility should be high
+
+console.log("shravani")
+
+
+let name = "shravani"; // string
+let age = 20 // number
+let isLoggedIn = true; //boolean 
+let state  // undefined
+let states = null; // null
+
+// number => 2 to the power 53
+// bigint
+// string => "" / ''
+// boolean => true/ false
+// null =>  standalone value
+// undefined =>
+// symbol =>unique
+
+// object  
+
+console.log(typeof 20);
+
+console.log(typeof null); // null is object in js IMP*
+
+console.log(typeof undefined);  // undefined
